5c4cadf6c771eba58a89c8da8706a78fb7ea60dc
SamanthaAdrichem <samantha@adrichem.nu>
2019-11-22 15:57:04 +0100

    Fix for styleUrls as requires at the top of the file
    Fix for styleUrls as requires at the top of the file

--- index.js
+++ index.js
@@ -36,20 +36,23 @@ module.exports = function(source, sourcemap) {
     // Not cacheable during unit tests;
   this.cacheable && this.cacheable();
 
+  let requires = '';
   var newSource = source.replace(templateUrlRegex, function (match, url) {
-                 // replace: templateUrl: './path/to/template.html'
-                 // with: template: require('./path/to/template.html')
-                 // or: templateUrl: require('./path/to/template.html')
-                 // if `keepUrl` query parameter is set to true.
-                 return templateProperty + ":" + replaceStringsWithRequires(url);
-               })
-               .replace(stylesRegex, function (match, urls) {
-                 // replace: stylesUrl: ['./foo.css', "./baz.css", "./index.component.css"]
-                 // with: styles: [require('./foo.css'), require("./baz.css"), require("./index.component.css")]
-                 // or: styleUrls: [require('./foo.css'), require("./baz.css"), require("./index.component.css")]
-                 // if `keepUrl` query parameter is set to true.
-                 return styleProperty + ":" + replaceStringsWithRequires(urls);
-               });
+     // replace: templateUrl: './path/to/template.html'
+     // with: template: require('./path/to/template.html')
+     // or: templateUrl: require('./path/to/template.html')
+     // if `keepUrl` query parameter is set to true.
+     return templateProperty + ":" + replaceStringsWithRequires(url);
+   })
+   .replace(stylesRegex, function (match, urls) {
+     // replace: stylesUrl: ['./foo.css', "./baz.css", "./index.component.css"]
+     // with: styles: [require('./foo.css'), require("./baz.css"), require("./index.component.css")]
+     // or: styleUrls: [require('./foo.css'), require("./baz.css"), require("./index.component.css")]
+     // if `keepUrl` query parameter is set to true.
+     requires += replaceStringsWithRequires(urls) + ';\n';
+     return 'styleUrls: []'; //
+   });
+  newSource = requires + '\n' + newSource
 
   // Support for tests
   if (this.callback) {
